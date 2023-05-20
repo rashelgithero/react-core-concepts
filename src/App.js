7de08874fa2aaef48be1f3da46072b2244db6f64
unknown <rashelrana4006@gmail.com>
2023-05-20 16:03:46 +0600

    new commit is done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const products = [
+    {name: " Key-board", price: ' $40' },
+    {name: ' Mouse', price: ' $30'},
+    {name: 'Charger', price: ' $10'},
+    {name: 'Mobile', price: ' $100'},
+    {name: 'Laptop', price: ' $200'}
+
+  ]
+  
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p> 
+        <p>I am a react person</p>
+        <Counter></Counter>
+        <Users></Users>
+        {
+          products.map(productive => <Product product = {productive}></Product> )
+        }
+        
+               
+         
       </header>
     </div>
   );
 }
+function Counter(){
+  const [count, setCount] = useState(9); 
+  
+  return (
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={() =>setCount(count + 1)} style={{backgroundColor: 'cyan'}}>Increase</button>
+      <br />
+      <button onMouseMove={() =>setCount(count - 1)} style={{backgroundColor: 'orange'}}>Decrease</button>
+    </div>
+  )
+}
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h3>Dynamic Users:</h3>
+      
+        
+        
+        {users.map(user => <li>{user.phone}</li> )}
+      
+    </div>
+  )
+}
+
+function Product (props){
+  const productStyle ={
+    border: '2px solid grey',
+    borderRadius: '5px',
+    height: '250px',
+    width: '300px',
+    margin: '20px',
+    padding: '10px 20px' 
+  }
+
+  const {name, price} = props.product;
+  console.log(name, price)
+
+
+  return (
+    <div style={productStyle}>
+      <h2>name: {name} </h2>
+      <p>price: {price} </p>
+      <button style={{backgroundColor: 'cyan', fontSize: '20px'}}>Buy Now</button>
+    </div>
+  )
+}
+
+ 
+
+
 
 export default App;
